574b6fbfba07e5ad3f6e1405815e321af70c5cdc
Shiga Gangadharan <shiga@tekarch.com>
2025-01-14 16:43:20 -0500

    created buildspec.file updated

--- AccountMSv/src/main/java/com/tekarch/AccountMSv/Repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tekarch.AccountMSv.Repository;
-import com.tekarch.AccountMSv.Models.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Accounts, Long> {
-
-}
