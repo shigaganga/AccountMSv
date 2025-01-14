6f8b44a0f48f837f05f21fff1f8edb229dc8c145
Shiga Gangadharan <shiga@tekarch.com>
2025-01-14 16:44:00 -0500

    created buildspec.file updated

---
+++ src/main/java/com/tekarch/AccountMSv/AccountMSvApplication.java
@@ -0,0 +1,13 @@
+package com.tekarch.AccountMSv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMSvApplication.class, args);
+	}
+
+}
